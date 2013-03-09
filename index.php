35b8c0bceee79a3a07e52371495dc9ad4e4c0e9b
mappleconfusers <mapc@cupdev.net>
2013-03-09 12:32:33 +0100

    Replace header with logo

--- index.php
+++ index.php
@@ -5,12 +5,8 @@
 				if ($blog_hero){
 			?>
 			<div class="clearfix row-fluid">
-				<div class="hero-unit">
-				
-					<h1><?php bloginfo('title'); ?></h1>
-					
-					<p><?php bloginfo('description'); ?></p>
-				
+                                <div class="hero-unit">
+                                    <img src="https://cupdev.net/~mapc/felswerk/logo/felswerk%20V1_final_wide_color256x.png"> 
 				</div>
 			</div>
 			<?php
